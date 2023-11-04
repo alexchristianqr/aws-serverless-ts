1b2a4a6f020c8b49b913dbfb69140c3656838dee
Alex Christian <alexchristianqr@gmail.com>
2023-11-04 09:48:01 -0500

    [updated] generic repository

--- src/core/repositories/generic-provider.repository.ts
+++ src/core/repositories/generic-provider.repository.ts
@@ -1,3 +1,7 @@
 export abstract class GenericProviderRepository<T> {
-  abstract create(data: T): Promise<T>
+  abstract create?(data: T): Promise<T>
+  abstract getAll?(): Promise<Array<T>>
+  abstract getById?(id: number): Promise<T | null>
+  abstract update?(id: number, data: T): Promise<boolean>
+  abstract delete?(id: number): Promise<boolean>
 }
--- src/users/domain/ports/output/user-output.repository.ts
+++ src/users/domain/ports/output/user-output.repository.ts
@@ -1,9 +1,5 @@
 import { GenericProviderRepository } from "../../../../core"
 
 export abstract class UserOutputRepository<T> extends GenericProviderRepository<T> {
-  abstract getById(id: number): Promise<T | null>
-  abstract getAll(): Promise<Array<T>>
-  abstract update(id: number, data: T): Promise<boolean>
-  abstract delete(id: number): Promise<boolean>
   abstract updateField(id: number, key: string, value: any): Promise<boolean>
 }
