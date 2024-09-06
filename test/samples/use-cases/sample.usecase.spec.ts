c760164fd784b5dac669592cedb15bc2ffebd1c3
Alex Christian <alexchristianqr@gmail.com>
2024-09-06 02:08:11 -0500

    refactor

--- test/samples/use-cases/sample.usecase.spec.ts
+++ test/samples/use-cases/sample.usecase.spec.ts
@@ -1,7 +1,7 @@
 import { SampleUsecase } from "../../../src/samples/application/use-cases/sample.usecase";
 import { SampleLocalRepository } from "../../../src/samples/infrastructure/database/repositories/sample-local.repository";
 import { CreateSampleDto, ICreateSampleDto } from "../../../src/samples/application/dtos/create-sample.dto";
-import { CI_InvokeCommandInput, CV_LogType, LambdafunctionAdapterService } from "../../../src/@common";
+// import { CI_InvokeCommandInput, CV_LogType, LambdafunctionAdapterService } from "../../../src/@common";
 
 describe("Sample use case", () => {
   let repository: SampleLocalRepository;
@@ -13,35 +13,34 @@ describe("Sample use case", () => {
     usecase = new SampleUsecase(repository);
   });
 
-  it("All samples test", async () => {
-    const input: CI_InvokeCommandInput = {
-      FunctionName: "AWSTEMPLATETS-dev-USERSMODULELF", //arn:aws:lambda:us-east-1:744984787614:function:user-lambdafunctiontr
-      InvocationType: "Event", // RequestResponse, Event
-      Payload: JSON.stringify({
-        httpMethod: "GET",
-        resource: "/users"
-      }),
-      LogType: CV_LogType.Tail
-    };
-    const respuestaInvocada: any = await LambdafunctionAdapterService.invokeFunction(input);
-    console.log(JSON.stringify(respuestaInvocada));
-  });
+  // it("All samples test", async () => {
+  //   const input: CI_InvokeCommandInput = {
+  //     FunctionName: "AWSTEMPLATE-dev-SAMPLES_MODULE_LF", // arn:aws:lambda:us-east-1:744984787614:function:user-lambdafunctiontr
+  //     InvocationType: "Event", // RequestResponse, Event
+  //     Payload: JSON.stringify({
+  //       httpMethod: "GET",
+  //       resource: "/samples"
+  //     }),
+  //     LogType: CV_LogType.Tail
+  //   };
+  //   const respuestaInvocada: any = await LambdafunctionAdapterService.invokeFunction(input);
+  //   console.log(JSON.stringify(respuestaInvocada));
+  // });
 
   it("Create sample test", async () => {
     const sample: ICreateSampleDto = new CreateSampleDto({
       id: 1,
-      name: "Alex",
-      lastname: "Quispe",
-      age: "28"
+      title: "Alex",
+      description: "Quispe"
     });
 
     result = await usecase.createSample(sample);
-    console.log({ result });
+    console.log(result);
     expect(result).toEqual(sample);
     const id = result?.id;
 
     result = await repository.getById(id);
-    console.log({ result });
+    console.log(result);
     expect(result).toEqual(sample);
   });
 });
